0f369012022723afa2f94132e7520edc67c71e7a
rob587 <robertocamm02@yahoo.com>
2026-06-30 11:21:04 +0200

    feat: add route for retrieving workflow statistics

--- server/routing/workflow.js
+++ server/routing/workflow.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
   analyzeWorkflow,
   getWorkflows,
+  getStats,
 } = require("../controllers/workflowController");
 
 console.log("Caricamento delle rotte di Workflow");
 
 router.post("/analyze", analyzeWorkflow);
 router.post("/all", getWorkflows);
+router.get("/stats", getStats);
 
 module.exports = router;
